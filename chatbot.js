c17eeb3c35a7b2fe60f37cc17fc0e489881ff422
Farhan Abbas <fabbas.ambition@gmail.com>
2024-05-01 20:17:38 +0530

    updated

--- chatbot.js
+++ chatbot.js
@@ -49,7 +49,7 @@ async function sendMessage() {
 }
 
 async function getMessages(message) {
-	const backendEndpoint = "https://mike-tyson-chatbot.vercel.app/api/chat.py";
+	const backendEndpsoint = "https://mike-tyson-chatbot.vercel.app/";
 	try {
 		const response = await fetch(backendEndpoint, {
 			method: "POST",
